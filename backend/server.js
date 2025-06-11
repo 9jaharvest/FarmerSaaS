833d3bf5ecee54db873951860a3ab267c07c019b
9jaharvest <9jaharvest2025@gmail.com>
2025-06-11 14:56:19 -0400

    Update server.js
    update to port

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,7 @@
 // Placeholder for server.js
 console.log('Backend Server');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
